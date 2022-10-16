3002ab54418ad2098f6301852016a1ca29d2b43d
Luigi Lupini <luigi.lupini@gmail.com>
2022-10-16 20:57:16 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Base from "./components/Base";
+import Toppings from "./components/Toppings";
+import Order from "./components/Order";
+
+function App() {
+  return <div>hello pizza</div>;
+}
+
+export default App;
