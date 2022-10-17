95c837f2d9c2a7935722d0e4d38be86860a2e676
Luigi Lupini <luigi.lupini@gmail.com>
2022-10-17 14:24:16 +0200

    #6 - Applying keyframes into our variants

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,31 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { motion } from "framer-motion";
-// Our initial starting/entering (from) animation
-const start = { opacity: 0 };
-// We animate (to) the ending/leaving animation
-const end = { opacity: 1 };
-// Lastly transition attribute options:
-const options = { delay: 1, duration: 1.25 };
+
+// # Keyframes: 🎞️
+// Variants also allow us to setup our `animate` prop to execute in a set/series
+// of keyframes. This animates through each value in rather a sequence.
+// https://www.framer.com/docs/animation/##keyframes
+const buttonVariants = {
+  hover: {
+    scale: [1, 1.1, 1, 1.1, 1, 1.1, 1], // represents keyframes :)
+    textShadow: "0px 0px 1px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+  },
+};
 
 const Home = () => {
   return (
     <motion.div
       className="home container"
-      initial={start}
-      animate={end}
-      transition={options}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1, duration: 1.25 }}
     >
       <h2>Welcome ragazzi :)</h2>
       <Link to="/base">
-        {/* Properties to animate to while the hover gesture is recognised. */}
-        <motion.button
-          whileHover={{
-            scale: 1.1,
-            textShadow: "0px 0px 1px rgb(255,255,255)",
-            boxShadow: "0px 0px 8px rgb(255,255,255)",
-          }}
-        >
+        {/* Properties to animate to while the hover gesture is recognized. */}
+        <motion.button variants={buttonVariants} whileHover="hover">
           Create Your Pizza
         </motion.button>
       </Link>
